be8a46ff394d0e7ed34e9d738a3b99a03fc4d4b6
Chandra Kuchibhotla <ckuchibh@gmail.com>
2025-05-21 16:51:55 +0530

    Add files via upload

---
+++ java/hello.java
@@ -0,0 +1,19 @@
+
+public class hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello world!");
+		String name = "Abhiram";
+		System.out.println(name);
+		name = "Venkat";
+		
+		System.out.println(name.toUpperCase());
+		System.out.println(name);
+		
+		
+		
+
+	}
+
+}
